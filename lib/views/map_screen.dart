cd5548623346ec302e32a10792094ce4f0255136
akhandsingh <akhandsingh0707@gmail.com>
2025-12-06 11:18:39 +0530

    fix map issue

--- lib/views/map_screen.dart
+++ lib/views/map_screen.dart
@@ -1,70 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
 import 'package:flutter_location_app/viewmodels/location_viewmodel.dart';
 
-class MapScreen extends ConsumerWidget {
+class MapScreen extends ConsumerStatefulWidget {
   const MapScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends ConsumerState<MapScreen> {
+  GoogleMapController? _mapController;
+  Set<Marker> _markers = {};
+  Set<Polyline> _polylines = {};
+  LatLng? _currentLatLng;
+
+  @override
+  Widget build(BuildContext context) {
     final locationState = ref.watch(locationViewModelProvider);
 
-    Set<Marker> markers = {};
-    Set<Polyline> polylines = {};
+    // Schedule update after build
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (locationState.currentLocation.value != null) {
+        final location = locationState.currentLocation.value!;
+        final newLatLng = LatLng(location.latitude, location.longitude);
 
-    LatLng? currentLocationLatLng;
+        setState(() {
+          _currentLatLng = newLatLng;
 
-    locationState.currentLocation.whenData((location) {
-      if (location != null) {
-        currentLocationLatLng = LatLng(location.latitude, location.longitude);
-        markers.add(
-          Marker(
-            markerId: const MarkerId('currentLocation'),
-            position: currentLocationLatLng!,
-            infoWindow: InfoWindow(
-              title: 'Current Location',
-              snippet: 'Lat: \${location.latitude}, Lng: \${location.longitude}',
+          _markers = {
+            Marker(
+              markerId: const MarkerId('currentLocation'),
+              position: newLatLng,
+              infoWindow: InfoWindow(
+                title: 'Current Location',
+                snippet:
+                'Lat: ${location.latitude}, Lng: ${location.longitude}',
+              ),
             ),
-          ),
-        );
+          };
+
+          if (locationState.history.isNotEmpty) {
+            final historyLatLngs = locationState.history
+                .map((loc) => LatLng(loc.latitude, loc.longitude))
+                .toList();
+
+            _polylines = {
+              Polyline(
+                polylineId: const PolylineId('locationHistory'),
+                points: historyLatLngs,
+                color: Colors.blue,
+                width: 5,
+              ),
+            };
+          }
+
+          // Move camera
+          _mapController?.animateCamera(CameraUpdate.newLatLng(newLatLng));
+        });
       }
     });
 
-    if (locationState.history.isNotEmpty) {
-      final historyLatLngs = locationState.history
-          .map((loc) => LatLng(loc.latitude, loc.longitude))
-          .toList();
-
-      polylines.add(
-        Polyline(
-          polylineId: const PolylineId('locationHistory'),
-          points: historyLatLngs,
-          color: Colors.blue,
-          width: 5,
-        ),
-      );
-    }
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Map View'),
-      ),
-      body: currentLocationLatLng == null
+      appBar: AppBar(title: const Text('Map View')),
+      body: _currentLatLng == null
           ? const Center(child: CircularProgressIndicator())
           : GoogleMap(
-              initialCameraPosition: CameraPosition(
-                target: currentLocationLatLng!,
-                zoom: 15,
-              ),
-              markers: markers,
-              polylines: polylines,
-              myLocationEnabled: true,
-              myLocationButtonEnabled: true,
-            ),
+        initialCameraPosition:
+        CameraPosition(target: _currentLatLng!, zoom: 15),
+        markers: _markers,
+        polylines: _polylines,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        onMapCreated: (controller) => _mapController = controller,
+      ),
     );
   }
 }
-
-
